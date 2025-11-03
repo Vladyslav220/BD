e8dca4148d56f573442ba9e18a662151aab88be5
Vladyslav220 <73350392+Vladyslav220@users.noreply.github.com>
2025-11-03 02:06:56 +0200

    Update config.py

--- lab5/app/config.py
+++ lab5/app/config.py
@@ -1,2 +1,6 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:root@127.0.0.1:3306/parkingdb'
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL",
+        "mysql+pymysql://admin:NewStrongPass!123@database-lab-1.c3u0c6k0idq9.eu-north-1.rds.amazonaws.com:3306/parkingdb"
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
