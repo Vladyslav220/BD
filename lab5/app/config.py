c5fd17c9a32476a6b44294054f4c36abb281687e
Vladyslav220 <73350392+Vladyslav220@users.noreply.github.com>
2025-11-03 02:37:41 +0200

    Update config.py

--- lab5/app/config.py
+++ lab5/app/config.py
@@ -1,6 +1,9 @@
+import os
+
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv(
         "DATABASE_URL",
         "mysql+pymysql://admin:NewStrongPass!123@database-lab-1.c3u0c6k0idq9.eu-north-1.rds.amazonaws.com:3306/parkingdb"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv("SECRET_KEY", "change-me")
